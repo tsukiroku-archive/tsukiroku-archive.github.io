79d743be5b1e69d739cc7f6fe662a96f7a6f81bf
rwdv <59119316+rwdv@users.noreply.github.com>
2022-02-25 19:29:36 +0900

    Update `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn wasm_init() {
-    alert("ㅇㅇ");
+pub fn wasm_init() -> Result<(), JsValue> {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
+
+    let element = document.create_element("test")?;
+    element.set_text_content(Some("테스트"));
+
+    body.append_child(&element)?;
+    Ok(())
 }
