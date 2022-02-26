8f26a3049be35433c704e7bdafeacd8deb0b4a4a
rwdv <59119316+rwdv@users.noreply.github.com>
2022-02-26 19:36:46 +0900

    Update `view.rs`

--- src/view.rs
+++ src/view.rs
@@ -12,17 +12,40 @@ pub fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 pub fn update(_: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {}
 
 pub fn view(_: &Model) -> Node<Msg> {
-    div![div![
+    div![
         style! {
-            St::Display => "flex",
-            St::JustifyContent => "center",
-            St::AlignItems => "center",
+            St::FontFamily => "BMJUA"
         },
-        h1! {
-            "rwdv",
+        div![
             style! {
+                St::Display => "grid",
+                St::PlaceItems => "center",
+                St::MinHeight => "70vh"
+            },
+            a![
+                attrs! { At::Href => "https://youtu.be/jYvMWxKuCvQ" },
+                img![
+                    attrs! {At::Src => "https://cdn.discordapp.com/attachments/889855991246626869/947073055186886666/17ebd9aa75013cb79.gif", At::Alt => "안아줘요"}
+                ]
+            ],
+        ],
+        div![
+            style! {
+                St::Display => "grid",
+                St::PlaceItems => "center",
+                St::MinHeight => "20vh",
+
+                St::FontSize => "30px",
                 St::Color => "white"
+            },
+            p! {
+                "By ",
+                a! {
+                    "rwdv",
+                    attrs! { At::Href => "https://github.com/rwdv" },
+                    style! { St::TextDecoration => "none", St::Color => "rgb(255, 190, 200)" }
+                }
             }
-        }
-    ]]
+        ]
+    ]
 }
