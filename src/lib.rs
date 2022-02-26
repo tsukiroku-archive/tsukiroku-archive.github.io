be0a1e4152aee71ba3e5fb33cd9b656a4e1d2754
rwdv <59119316+rwdv@users.noreply.github.com>
2022-02-26 10:11:36 +0900

    (test)

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use ws::default as wd;
 pub fn wasm_init() -> Result<(), JsValue> {
     let w = wd();
     let element = w.document.create_element("test")?;
-    element.set_text_content(Some("테스트"));
+    element.set_text_content(Some("testt"));
 
     w.body.append_child(&element)?;
     Ok(())
