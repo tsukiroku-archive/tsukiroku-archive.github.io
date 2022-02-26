33eb4a0f61db1ca0b833bc884d7223626b8f2e83
rwdv <59119316+rwdv@users.noreply.github.com>
2022-02-26 10:47:28 +0900

    Update `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use ws::default as wd;
 #[wasm_bindgen]
 pub fn wasm_init() -> Result<(), JsValue> {
     let w = wd();
-    let element = w.document.create_element("test")?;
+    let element = w.document.create_element("h1")?;
     element.set_text_content(Some("test _"));
 
     w.body.append_child(&element)?;
