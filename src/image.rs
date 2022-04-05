86715c2bfdbf2998246f35ea0a706f69c98362c1
Rusty cat <tsukiroku@yahoo.com>
2022-04-05 21:34:24 +0900

    Delete image.rs

--- src/image.rs
+++
@@ -1,8 +0,0 @@
-use rand::{prelude::SliceRandom, thread_rng};
-
-pub fn f<'a>() -> &'a str {
-    let mut i = ["./resource/1.gif", "./resource/2.gif"];
-    i.shuffle(&mut thread_rng());
-    crate::log(format!("image: {}", i[0]).as_str());
-    i[0].clone()
-}
