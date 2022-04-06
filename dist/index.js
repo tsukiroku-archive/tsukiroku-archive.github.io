c047b444f904d4d83efd05acbd7a724103965d22
Rusty cat <tsukiroku@yahoo.com>
2022-04-06 20:16:42 +0900

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,63 @@
 "use strict";
-const execute = (command, _) => {
+let input = document.querySelector("input");
+const terminal = document.querySelector("#terminal");
+const commands = ["help", "github", "twitter", "clear", "echo"].sort();
+const execute = (command, args) => {
     if (!command)
-        return { execute: () => "" };
+        return { execute: () => null, command: command, withNewline: false };
     switch (command.toLowerCase()) {
         case "help":
             return {
                 execute: () => {
-                    return "help <br>";
+                    return `<br>${commands.join("<br>")}<br>`;
                 },
+                command: "help",
+                withNewline: true,
             };
         case "github":
             return {
                 execute: () => {
-                    return '<a href="https://github.com/tsukiroku">Click here.</a> <br>';
+                    return '<br><a href="https://github.com/tsukiroku">Click here.</a><br>';
                 },
+                command: "github",
+                withNewline: true,
+            };
+        case "twitter":
+            return {
+                execute: () => {
+                    return '<br><a href="https://twitter.com/tsukiroku_t">Click here.</a><br>';
+                },
+                command: "twitter",
+                withNewline: true,
+            };
+        case "clear":
+            return {
+                execute: () => {
+                    terminal.innerHTML = "";
+                    init();
+                    return null;
+                },
+                command: "clear",
+                withNewline: false,
+            };
+        case "echo":
+            return {
+                execute: () => {
+                    return `${args.join(" ")}`;
+                },
+                command: "echo",
+                withNewline: true,
             };
         default:
             return {
                 execute: () => {
-                    return `${command}: command not found <br>`;
+                    return `${command}: command not found.`;
                 },
+                command: command,
+                withNewline: true,
             };
     }
 };
-let input = document.querySelector("input");
-const terminal = document.body;
 const element = `
 <span style="color: #00c800">root@User</span
 ><span style="color: #ffffff">:</span><span style="color: #106dca">~</span
@@ -37,10 +70,12 @@ const init = () => {
     });
     input?.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
-            const _i = input?.value;
+            const [a, ...b] = input.value.split(" ");
+            const command = execute(a, b);
+            const commandExecute = command.execute();
+            const commandResult = command.command;
             input?.remove();
-            const [c, ...a] = _i.split(" ");
-            terminal.innerHTML += `<span style="color: rgb(170, 170, 170);">${_i}<span>\n<br> ${execute(c, a).execute()} ${element}`;
+            terminal.innerHTML += `<span style="color: rgb(170, 170, 170);">${commandExecute ? `${commandResult} ${b.join(" ")}` : ""}<span>\n<br> ${commandExecute ?? ""} ${command.withNewline ? "<br>" : ""} ${element}`;
             input = document.querySelector("input");
             input?.focus();
             init();
