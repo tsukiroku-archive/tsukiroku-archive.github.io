47d8d6ff4b2f39bf43cd51ec71d791f43638bb73
Rusty cat <tsukiroku@yahoo.com>
2022-04-07 19:01:29 +0900

    Update command.js

--- dist/command.js
+++ dist/command.js
@@ -1,5 +1,14 @@
 import * as commandExecute from "./commands/index.js";
-const commands = ["help", "github", "twitter", "clear", "echo"].sort();
+const commands = ["help", "github", "twitter", "clear", "echo", "bio"]
+    .sort()
+    .map((t) => {
+    if (["github", "twitter", "bio"].includes(t)) {
+        return `<span style="color: #34a6ff"><strong>${t}</strong></span>`;
+    }
+    else {
+        return `<span style="color: #FFFFFF">${t}</span>`;
+    }
+});
 const execute = (command, args) => {
     if (!command)
         return { execute: () => null, command: command, withNewline: false };
@@ -14,6 +23,8 @@ const execute = (command, args) => {
             return commandExecute.clear.on(command, args);
         case "echo":
             return commandExecute.echo.on(command, args);
+        case "bio":
+            return commandExecute.bio.on(command, args);
         default:
             return commandExecute.on(command, args);
     }
