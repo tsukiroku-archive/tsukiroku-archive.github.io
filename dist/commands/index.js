16b89c3b2f773a8b261272fab09215046d3d63e8
Rusty cat <tsukiroku@yahoo.com>
2022-04-07 17:24:18 +0900

    Create index.js

---
+++ dist/commands/index.js
@@ -0,0 +1,16 @@
+import * as help from "./help.js";
+import * as github from "./github.js";
+import * as twitter from "./twitter.js";
+import * as clear from "./clear.js";
+import * as echo from "./echo.js";
+const on = (command, args) => {
+    return {
+        execute: () => {
+            return `${command}: command not found.`;
+        },
+        command: command,
+        withNewline: true,
+    };
+};
+export { on };
+export { help, github, twitter, clear, echo };
