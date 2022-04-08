e2db2507f3563700dc4899308a85bc843dc40162
Rusty cat <tsukiroku@yahoo.com>
2022-04-08 15:49:31 +0900

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,6 +4,7 @@ import * as twitter from "./twitter.js";
 import * as clear from "./clear.js";
 import * as echo from "./echo.js";
 import * as bio from "./bio.js";
+import * as debug from "./debug.js";
 
 import { ICommand } from "../command.js";
 
@@ -19,4 +20,4 @@ const on = (command: string, args: string[]): ICommand => {
 
 export { on };
 
-export { help, github, twitter, clear, echo, bio };
+export { help, github, twitter, clear, echo, bio, debug };
