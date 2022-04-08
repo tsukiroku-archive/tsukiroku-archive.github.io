14bdc954025f6c613c6dc343a8d85d3b8bc879b6
Rusty cat <tsukiroku@yahoo.com>
2022-04-08 16:38:15 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import { init } from "./terminal.js";
+import { init, input } from "./terminal.js";
 
 init();
+
+window.addEventListener("resize", () => {
+  input!.width = window.innerWidth - window.innerWidth / 5;
+});
