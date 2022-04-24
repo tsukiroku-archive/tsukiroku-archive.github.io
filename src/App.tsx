367e006ed2f3ec4420571747cdda0d8981b7e479
tsukiroku <tsukiroku@yahoo.com>
2022-04-24 15:27:09 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const App = () => {
   return (
     <div style={{ textAlign: "center", color: "rgb(230, 230, 230)" }}>
-      <h1>공사중</h1>
+      <h1>공사중입니다</h1>
     </div>
   );
 };
