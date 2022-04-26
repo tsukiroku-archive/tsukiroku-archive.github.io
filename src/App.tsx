e0c29c610a9c36b466915da8615f918bb31a1889
tsukiroku <tsukiroku@yahoo.com>
2022-04-26 22:30:44 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "../resource/style.css";
 import React from "react";
 
+import useWebAnimations, { bounce } from "@wellyshen/use-web-animations";
+
 const App = () => {
-  return (
-    <div style={{ textAlign: "center", color: "rgb(230, 230, 230)" }}>
-      <h1>공사중입니다</h1>
-    </div>
-  );
+    const { ref } = useWebAnimations({ ...bounce });
+
+    return (
+        <div
+            style={{ textAlign: "center", color: "rgb(230, 230, 230)" }}
+            ref={ref as React.RefObject<HTMLDivElement>}
+        >
+            <h1>공사중입니다</h1>
+        </div>
+    );
 };
 
 export default App;
