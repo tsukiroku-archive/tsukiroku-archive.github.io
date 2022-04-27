0fa581ea9f63a04c1eae3fcfbae98e560b1d3e2c
tsukiroku <tsukiroku@yahoo.com>
2022-04-27 19:49:46 +0900

    Create aboutme.tsx

---
+++ src/components/aboutme.tsx
@@ -0,0 +1,31 @@
+import "../../resource/style.css";
+import React, { useEffect } from "react";
+
+import useWebAnimations, { headShake } from "@wellyshen/use-web-animations";
+
+const AboutMe = () => {
+    const { ref: textRef } = useWebAnimations<HTMLDivElement>({
+        keyframes: [
+            { transform: "none", opacity: 0, offset: 0 },
+            { transform: "none", opacity: 1, offset: 1 },
+        ],
+        animationOptions: {
+            delay: 2300,
+            duration: 500,
+            fill: "forwards",
+        },
+    });
+    return (
+        <div>
+            <div className="text">
+                <h3 className="aboutme" ref={textRef}>
+                    The quick brown fox jumps over the lazy dog
+                    <br />
+                    다람쥐 헌 쳇바퀴에 타고파
+                </h3>
+            </div>
+        </div>
+    );
+};
+
+export default AboutMe;
