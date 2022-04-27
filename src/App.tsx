63fe4acffa966997d2c58cfb3287e49291877329
tsukiroku <tsukiroku@yahoo.com>
2022-04-27 19:49:44 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
-import "../resource/style.css";
-import React, { useEffect } from "react";
-
-import useWebAnimations, { headShake } from "@wellyshen/use-web-animations";
+import React, { Component } from "react";
+import Title from "./components/title";
+import AboutMe from "./components/aboutme";
 
 const App = () => {
-    const { ref: ref, animate } = useWebAnimations<HTMLDivElement>({
-        ...headShake,
-    });
-    const { ref: textRef } = useWebAnimations<HTMLDivElement>({
-        keyframes: { opacity: ["0", "1"] },
-        animationOptions: {
-            delay: 1000,
-            duration: 500,
-            fill: "forwards",
-        },
-    });
-    const { ref: block } = useWebAnimations<HTMLDivElement>({
-        keyframes: { width: ["0", "100%", "0"], left: ["0", "0", "100%"] },
-        animationOptions: {
-            duration: 2000,
-            fill: "forwards",
-            easing: "cubic-bezier(0.74, 0.06, 0.4, 0.92)",
-        },
-    });
-    useEffect(() => {
-        document.addEventListener("mousedown", () => {
-            animate({ ...headShake });
-        });
-    }, [animate]);
     return (
         <div>
-            <div className="text">
-                <div className="block" ref={block} />
-                <div ref={ref}>
-                    <h1 ref={textRef}>공사중입니다.</h1>
-                </div>
-            </div>
+            <Title />
+            <AboutMe />
         </div>
     );
 };
