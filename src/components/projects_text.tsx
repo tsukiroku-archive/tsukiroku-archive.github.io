2ea5bdf3d694ffc7910a23cb3cb166edd5a863f0
tsukiroku <tsukiroku@yahoo.com>
2022-04-28 19:15:27 +0900

    Create projects_text.tsx

---
+++ src/components/projects_text.tsx
@@ -0,0 +1,30 @@
+import useWebAnimations from "@wellyshen/use-web-animations";
+import React from "react";
+
+interface IProps {
+    name: string;
+    description: string;
+    translate?: number;
+}
+
+const Project = (props: IProps) => {
+    const { ref: textRef } = useWebAnimations<HTMLDivElement>({
+        keyframes: [
+            {
+                transform: `translate(0, ${30 + (props.translate ?? 0)}%)`,
+                offset: 1,
+            },
+        ],
+        animationOptions: {
+            fill: "forwards",
+        },
+    });
+
+    return (
+        <div className="text" ref={textRef}>
+            <p className="project">{props.name} — </p><p className="project-description">{props.description}</p>
+        </div>
+    );
+};
+
+export default Project;
