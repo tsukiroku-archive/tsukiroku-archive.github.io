7a29f68c540947be9f65f8ec97304ec40235e389
tsukiroku <tsukiroku@yahoo.com>
2022-04-28 19:15:22 +0900

    Update aboutme.tsx

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -5,12 +5,12 @@ import useWebAnimations from "@wellyshen/use-web-animations";
 const AboutMe = () => {
     const { ref: textRef } = useWebAnimations<HTMLDivElement>({
         keyframes: [
-            { transform: "none", opacity: 0, offset: 0 },
-            { transform: "none", opacity: 1, offset: 1 },
+            { transform: "translate3d(0, 500%, 0)", opacity: 0, offset: 0 },
+            { transform: "translate3d(0, 0, 0)", opacity: 1, offset: 1 },
         ],
         animationOptions: {
-            delay: 2800,
-            duration: 500,
+            delay: 3500,
+            duration: 450,
             fill: "forwards",
         },
     });
@@ -19,9 +19,11 @@ const AboutMe = () => {
         <div>
             <div className="text">
                 <h3 className="aboutme" ref={textRef}>
-                    The quick brown fox jumps over the lazy dog
+                    반갑습니다!
                     <br />
-                    다람쥐 헌 쳇바퀴에 타고파
+                    주로 타입스크립트를 사용하며, C/C++, Rust 등도 어느정도 다룰 수 있습니다.
+                    <br />
+                    또한 Ps, Ai, Ae, Pr 등을 사용하여, 디자인/미디어 작업도 가능합니다.
                 </h3>
             </div>
         </div>
