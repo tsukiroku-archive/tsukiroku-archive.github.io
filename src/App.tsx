4efdc74066c036f937dca207377fc01ad4f1bfae
tsukiroku <tsukiroku@yahoo.com>
2022-05-21 19:38:08 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Title from "./components/title";
-import AboutMe from "./components/aboutme";
-import Projects from "./components/projects";
-import "../resource/style.css";
+import React from 'react';
+import Title from './components/title';
+import AboutMe from './components/aboutme';
+import Projects from './components/projects';
+import '../resource/style.css';
 
 const App = () => {
     return (
