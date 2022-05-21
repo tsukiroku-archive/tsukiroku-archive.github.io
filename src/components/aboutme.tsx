56098c83da0c174a43d9b41769861b1476825b0a
tsukiroku <tsukiroku@yahoo.com>
2022-05-21 19:38:10 +0900

    Update aboutme.tsx

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import useWebAnimations from "@wellyshen/use-web-animations";
+import useWebAnimations from '@wellyshen/use-web-animations';
 
 const AboutMe = () => {
     const { ref: textRef } = useWebAnimations<HTMLDivElement>({
         keyframes: [
-            { transform: "translate3d(0, 500%, 0)", opacity: 0, offset: 0 },
-            { transform: "translate3d(0, 0, 0)", opacity: 1, offset: 1 },
+            { transform: 'translate3d(0, 500%, 0)', opacity: 0, offset: 0 },
+            { transform: 'translate3d(0, 0, 0)', opacity: 1, offset: 1 },
         ],
         animationOptions: {
             delay: 3500,
             duration: 450,
-            fill: "forwards",
+            fill: 'forwards',
         },
     });
 
