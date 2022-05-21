8832935d93461605926923e50cd0505c5ecbaa3c
tsukiroku <tsukiroku@yahoo.com>
2022-05-21 19:38:15 +0900

    Update title.tsx

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import useWebAnimations, { headShake } from "@wellyshen/use-web-animations";
+import useWebAnimations, { headShake } from '@wellyshen/use-web-animations';
 
 const Title = () => {
     const { ref: ref, animate } = useWebAnimations<HTMLDivElement>({
@@ -9,28 +9,28 @@ const Title = () => {
 
     const { ref: textRef } = useWebAnimations<HTMLDivElement>({
         keyframes: [
-            { transform: "none", opacity: 0, offset: 0 },
-            { transform: "none", opacity: 1, offset: 0.8 },
-            { transform: "translateY(-170%)", opacity: 1, offset: 1 },
+            { transform: 'none', opacity: 0, offset: 0 },
+            { transform: 'none', opacity: 1, offset: 0.8 },
+            { transform: 'translateY(-170%)', opacity: 1, offset: 1 },
         ],
         animationOptions: {
             delay: 1500,
             duration: 1500,
-            fill: "forwards",
+            fill: 'forwards',
         },
     });
 
     const { ref: block } = useWebAnimations<HTMLDivElement>({
-        keyframes: { width: ["0", "100%", "0"], left: ["0", "0", "100%"] },
+        keyframes: { width: ['0', '100%', '0'], left: ['0', '0', '100%'] },
         animationOptions: {
             duration: 2000,
-            fill: "forwards",
-            easing: "cubic-bezier(0.74, 0.06, 0.4, 0.92)",
+            fill: 'forwards',
+            easing: 'cubic-bezier(0.74, 0.06, 0.4, 0.92)',
         },
     });
 
     useEffect(() => {
-        document.addEventListener("mousedown", () => {
+        document.addEventListener('mousedown', () => {
             animate({ ...headShake });
         });
     }, [animate]);
